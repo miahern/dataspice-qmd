14b090d4b597726e41e4a3c93629cd62fc429e01
mia.s.herna <mia.s.hernandez-1@ou.edu>
2025-03-09 20:43:35 -0500

    update

--- data-qmd-activity 3.R
+++ data-qmd-activity 3.R
@@ -8,23 +8,3 @@ format:
   html:
     code-fold:true
 ---
-@fig-description
-```{r}
-#|label: fig-
-#|fig-cap: ""
-#| warning: false
-
-install.packages("ggplot2")
-library("ggplot2")
-ggplot(title, aes(x)y)
-
-#console
-library(quarto)
-quarto_render("document.qmd") # all formats
-quarto_render("document.qmd", output_format = "pdf")
-
-
-#terminal
-quarto render document.qmd # all formats
-quarto render document.qmd --to pdf
-quarto render document.qmd --to docx
